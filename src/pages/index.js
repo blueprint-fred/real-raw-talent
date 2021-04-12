56f42e3e547c935ecef67c8e50559652ba0a7681
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-13 01:13:50 +0800

    added carousel component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Carousel from '../components/Carousel'
 
 const HomePage = () => {
     return (
         <>
-        <Banner/>   
+        <Banner/>
+        <Carousel/>
         </>
     )
 }
