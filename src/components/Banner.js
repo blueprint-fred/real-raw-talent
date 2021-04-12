d269bf6b992b3c84b5b6560c80683386a0855388
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-13 02:33:44 +0800

    added actual working link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact'
 import React from 'react'
 import { Button, SHAPE, SIZE } from "baseui/button";
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -9,14 +10,15 @@ const Banner = () => {
                 <MDBCol className="text-center" md="8" lg="8">
                     <h1 className="cursive display-3">Talent's Secret Source</h1>
                     <h4 className="h4-responsive">The best app to manage your talent - developed for you.</h4>
-                    <p className="mb-5">Raw Real Talent manages peoples talent and delivers quality & perfection to the world.</p>
-                    <Button
-                    onClick={() => alert("click")}
-                    shape={SHAPE.pill}
-                    size={SIZE.large}
-                    >
-                        Try Raw Real Talent
-                    </Button>
+                    <p className="mb-5">Real Raw Talent manages peoples talent and delivers quality & perfection to the world.</p>
+                    <Link to="/discover">
+                        <Button
+                        shape={SHAPE.pill}
+                        size={SIZE.large}
+                        >
+                            Try Real Raw Talent
+                        </Button>
+                    </Link>
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
