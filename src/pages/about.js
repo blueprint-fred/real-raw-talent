ac7ecf72e55157de7fe8950616016b3a5a18c95f
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-13 02:41:38 +0800

    added dummy content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,168 @@
+import { MDBCol, MDBContainer, MDBRow, MDBIcon } from 'mdbreact'
 import React from 'react'
-
+import { Button, SHAPE, SIZE } from "baseui/button";
+import { Link } from 'react-router-dom';
+import {
+    Card,
+    StyledBody,
+  } from "baseui/card";
+  
 const AboutPage = () => {
     return (
-        <div>
-            
-        </div>
+        <MDBContainer className="py-5 my-5">
+            <MDBRow center>
+                <MDBCol className="text-center" md="8" lg="8">
+                    <h1 className="cursive display-4">RRT by Lucas Allen</h1>
+                    <h5 className="h5-responsive mb-4">
+                        Designed for aspiring artists to share their gifts -- say hello to Real Raw Talent!
+                    </h5>
+                    <p className="lead mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies diam consequat ligula ornare lacinia. Ut id augue ut magna euismod venenatis. Mauris sollicitudin, felis a imperdiet tempus, est nulla ornare quam, vitae sollicitudin purus nibh eu urna. In hac habitasse platea dictumst. Duis non tristique urna, non auctor augue. Nulla malesuada, lorem vestibulum tincidunt hendrerit, lectus arcu vulputate lacus, et semper mi felis eget enim. Vivamus vel erat eu sem rutrum commodo at sit amet leo. Nulla nec facilisis quam, ut cursus felis. Praesent vel turpis nulla. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <p className="lead mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies diam consequat ligula ornare lacinia. Ut id augue ut magna euismod venenatis. Mauris sollicitudin, felis a imperdiet tempus, est nulla ornare quam, vitae sollicitudin purus nibh eu urna. In hac habitasse platea dictumst. Duis non tristique urna, non auctor augue. Nulla malesuada, lorem vestibulum tincidunt hendrerit, lectus arcu vulputate lacus, et semper mi felis eget enim. Vivamus vel erat eu sem rutrum commodo at sit amet leo. Nulla nec facilisis quam, ut cursus felis. Praesent vel turpis nulla. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                    
+                    </p>
+                    <Link to="/discover">
+                        <Button
+                        shape={SHAPE.pill}
+                        size={SIZE.large}
+                        >
+                            Try Real Raw Talent
+                        </Button>
+                    </Link>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow className="py-5 my-5">
+                <MDBCol>
+                    <img src="https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="img-fluid"/>
+                    <h1 className="h1-responsive cursive py-4 text-center">Promoting Talent, Give Love</h1>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow className="pt-5 pb-3 mb-3 mt-5">
+                <MDBCol>
+                    <Card>
+                        <StyledBody>
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                        </StyledBody>
+                        {/* <StyledAction>
+                            <Button
+                            overrides={{
+                                BaseButton: { style: { width: "100%" } }
+                            }}
+                            >
+                            Button Label
+                            </Button>
+                        </StyledAction> */}
+                    </Card>
+                </MDBCol>
+                <MDBCol>
+                    <Card>
+                        <StyledBody>
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                        </StyledBody>
+                        {/* <StyledAction>
+                            <Button
+                            overrides={{
+                                BaseButton: { style: { width: "100%" } }
+                            }}
+                            >
+                            Button Label
+                            </Button>
+                        </StyledAction> */}
+                    </Card>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow className="py-3 my-3">
+                <MDBCol>
+                    <Card>
+                        <StyledBody>
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                        </StyledBody>
+                        {/* <StyledAction>
+                            <Button
+                            overrides={{
+                                BaseButton: { style: { width: "100%" } }
+                            }}
+                            >
+                            Button Label
+                            </Button>
+                        </StyledAction> */}
+                    </Card>
+                </MDBCol>
+                <MDBCol>
+                    <Card>
+                        <StyledBody>
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                            Proin ut dui sed metus pharetra hend rerit vel non
+                            mi. Nulla ornare faucibus ex, non facilisis nisl.
+                        </StyledBody>
+                        {/* <StyledAction>
+                            <Button
+                            overrides={{
+                                BaseButton: { style: { width: "100%" } }
+                            }}
+                            >
+                            Button Label
+                            </Button>
+                        </StyledAction> */}
+                    </Card>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow center className="py-5 my-5">
+                <MDBCol md="12" lg="12">
+                    <h5 className="h5-responsive text-center font-weight-bold">Follow us</h5>
+                </MDBCol>
+                <MDBCol md="6" lg="6">
+                    <MDBRow className="py-3 text-center">
+                        <MDBCol size="4" md="4" lg="4">
+                            <a className="black-text" href="#fb">
+                                <MDBIcon size="2x" fab icon="facebook-f" />
+                            </a>
+                        </MDBCol>
+                        <MDBCol size="4" md="4" lg="4">
+                            <a className="black-text" href="#twitter">
+                                <MDBIcon size="2x" fab icon="twitter" />
+                            </a>
+                        </MDBCol>
+                        <MDBCol size="4" md="4" lg="4">
+                            <a className="black-text" href="#ig">
+                                <MDBIcon fab size="2x" icon="instagram" />
+                            </a>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     )
 }
 
