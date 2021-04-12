e01b0534ed09903dbf842a5419384c054d6d858d
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-13 00:22:56 +0800

    added banner to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 const HomePage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Banner/>   
+        </>
     )
 }
 
